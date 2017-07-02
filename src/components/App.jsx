88d0cd137ed04f962fd80c85b3696d6afbddad8f
Kamyl <kamillcybulski@gmail.com>
2017-07-02 18:29:06 +0200

    Done stuff

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,85 @@
 import React from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import CircularProgress from 'material-ui/CircularProgress';
 
 injectTapEventPlugin();
 
-const App = () => (
-  <div> RQM!!!! </div>
+const Loader = () => (
+  <div className="loader">
+    <CircularProgress size={80} thickness={5} />
+  </div>
+);
+
+const Quote = ({ text, author }) => (
+  <div className="quote-container">
+    <div className="quote-text">{text}</div>
+    <div className="quote-author">{author}</div>
+  </div>
 )
+
+
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+      this.state = {
+        text: "",
+        author: ""
+      }
+  }
+
+  componentDidMount = () => {
+    this.getQuote();
+  }
+
+  getQuote = () => {
+    const url = "https://thesimpsonsquoteapi.glitch.me/quotes";
+
+    fetch(url)
+      .then(res => res.json())
+      .catch(err => {console.log(err)})
+      .then(data => {this.setState(
+          () => ({
+            text: data[0].quote,
+            author: data[0].character
+          })
+        )
+      })
+  }
+
+  tweetQuote = () => {
+    window.open(`https://twitter.com/home?status=${this.state.text} -${this.state.author}`);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Random Quote Generator</h2>
+        <Paper className="main-container" zDepth={2}>
+          {this.state.text 
+              ? <Quote text={this.state.text} author={this.state.author} /> 
+              : <Loader />}
+          <div className="buttons-container">
+            <RaisedButton 
+              primary 
+              label="New quote" 
+              style={{ margin: "10px"}}
+              onTouchTap={this.getQuote} />
+            <RaisedButton 
+              primary 
+              label="Tweet me" 
+              style={{ margin: "10px"}}
+              onTouchTap={this.tweetQuote} />
+          </div>
+        </Paper>
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { pink500, darkBlack } from 'material-ui/styles/colors';
 import App from './components/App';
 import './style.scss';
 
 const root = document.getElementById('root');
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: pink500,
+    textColor: darkBlack,
+  }
+});
+
 render(
   <AppContainer>
-    <App />
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <App />
+    </MuiThemeProvider>
   </AppContainer>,
   root,
 );
