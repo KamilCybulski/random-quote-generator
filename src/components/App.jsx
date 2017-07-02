62f7301d4f98e9dfb6e322b4ca594c0d90d3520a
Kamyl <kamillcybulski@gmail.com>
2017-07-02 16:54:29 +0200

    Initial commit. Setup

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
+const App = () => (
+  <div> RQM!!!! </div>
+)
+
+export default App;
